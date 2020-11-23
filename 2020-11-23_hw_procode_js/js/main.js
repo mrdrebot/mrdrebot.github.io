9756c7010a35ee7e823882a8ccd052b67e0d460f
mrdrebot <m.r.drebot.j@gmail.com>
2020-11-23 14:59:07 +0200

    change file main.js in home work 2020-11-23_hw_procode_js, deleted Math.round

--- 2020-11-23_hw_procode_js/js/main.js
+++ 2020-11-23_hw_procode_js/js/main.js
@@ -17,18 +17,20 @@ D = Math.pow(b,2) - 4 * a * c;
 findElem = document.querySelector(".main");
 
 if(D < 0) {
-    infornation = "<div>Вы ввели такие данные:</div><div>a = " + a + "</div><div>b = " + b + "</div><div>c = " + c + "</div><div>Уровнение имеет следующий вид:</div><div>" + a + "x^2 + " + b + "x + " + c +",</div><div> D = " + D + "</div><div>У Вашего уровнения нет корней!</div>";
+    infornation = "<div>Вы ввели такие данные:</div><div>a = " + a + "</div><div>b = " + b + "</div><div>c = " + c + "</div><div>Уровнение имеет следующий вид:</div><div>" + a + "x^2 + " + b + "x + " + c +",</div><div>Дискриминант</div><div> D = " + D + "</div><div>У Вашего уровнения нет корней!</div>";
     findElem.innerHTML = infornation;
 }
 else if(D === 0) {
     x = (b * -1)/(2 * a);
-    infornation = "<div>Вы ввели такие данные:</div><div>a = " + a + "</div><div>b = " + b + "</div><div>c = " + c + "</div><div>Уровнение имеет следующий вид:</div><div>" + a + "x^2 + " + b + "x + " + c +",</div><div>Дискременант</div><div>D = " + D + "</div><div>У Вашего уровнения один корень!</div><div>x = " + x + ".</div>";
+    infornation = "<div>Вы ввели такие данные:</div><div>a = " + a + "</div><div>b = " + b + "</div><div>c = " + c + "</div><div>Уровнение имеет следующий вид:</div><div>" + a + "x^2 + " + b + "x + " + c +",</div><div>Дискриминант</div><div>D = " + D + "</div><div>У Вашего уровнения один корень!</div><div>x = " + x + ".</div>";
     findElem.innerHTML = infornation;
 }
 else {
-    x1 = Math.round((((b * -1) + Math.sqrt(D)) / (2 * a)) * 2) / 2;
-    x2 = Math.round((((b * -1) - Math.sqrt(D)) / (2 * a)) * 2) / 2;
-    infornation = "<div>Вы ввели такие данные:</div><div>a = " + a + "</div><div>b = " + b + "</div><div>c = " + c + "</div><div>Уровнение имеет следующий вид:</div><div>" + a + "x^2 + " + b + "x + " + c +",</div><div>Дискременант</div><div>D = " + D + "</div><div>У Вашего уровнения два корня!</div><div>x1 = " + x1 + ",</div><div>x2 = " + x2 + ".</div>";
+    x1 = ((b * -1) + Math.sqrt(D)) / (2 * a);
+    x2 = ((b * -1) - Math.sqrt(D)) / (2 * a);
+    // x1 = Math.round((((b * -1) + Math.sqrt(D)) / (2 * a)) * 2) / 2;
+    // x2 = Math.round((((b * -1) - Math.sqrt(D)) / (2 * a)) * 2) / 2;
+    infornation = "<div>Вы ввели такие данные:</div><div>a = " + a + "</div><div>b = " + b + "</div><div>c = " + c + "</div><div>Уровнение имеет следующий вид:</div><div>" + a + "x^2 + " + b + "x + " + c +",</div><div>Дискриминант</div><div>D = " + D + "</div><div>У Вашего уровнения два корня!</div><div>x1 = " + x1 + ",</div><div>x2 = " + x2 + ".</div>";
     findElem.innerHTML = infornation;
 }
 
---
+++ 2020-11-23_hw_procode_js_v2/js/main.js
@@ -0,0 +1,8 @@
+let plotNumber = 3;
+let commonPlotSize = 300;
+let DefaultPlotSide = 10;
+let commonFenceNumber = 100;
+let NumberOneFence = null;
+
+NumberOneFence = commonFenceNumber / 2;
+
