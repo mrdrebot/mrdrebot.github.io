f072c897e0b75f497fedf38d90cf446faf5655da
mrdrebot <m.r.drebot.j@gmail.com>
2021-01-11 03:19:22 +0200

    finished home work 2021-01-07_cw_procode_parking

--- 2021-01-07_hw_parking_v2/js/main.js
+++ 2021-01-07_hw_parking_v2/js/main.js
@@ -1,27 +1,39 @@
 
-let places = 13;                                                        //  Кол-во парковочных мест
-let placesInRow = 10;                                                  //  Кол-во парковочных мест в ряду
-let parkingArr = new Array(places);
+let PLACES = 16;                                                        //  Кол-во парковочных мест
+let placesInRow = 10;                                                   //  Кол-во парковочных мест в ряду
+let parkingArr = [];
 let parkingRow = null;
 let parkingCol = null;
 let infoBoard = document.querySelector(".info-board");
 let placeHeight = null;
 let placeWidth = null;
-let freeCount = 0;
-let busyCount = 0;
 let freeSpaces = document.querySelector(".free-spaces");
-let busyPlaces = document.querySelector(".occupied-spaces");
+let occupiedSpaces = document.querySelector(".occupied-spaces");
 let curTime = document.querySelector(".current-time");
+let mainEl = document.querySelector(".main");
+let parkingPlacesArr;             // Создание массива парковочных мест
+let parFillFact = 0.9;
+
+//  Создание объектов со свойствами в массиве и добавление их в html-разметку
+const createPlaces = () => {
+    for(let i = 0; i < PLACES; i++) {
+        parkingArr.push({
+            id: i,
+            occupied: false,
+            time: "--:--",
+        });
+    }
+}
 
 //  Функция определения размеров (рядов и мест в ряду) оптимальной стоянки
 const createParkingSize = () => {
-    let tempFrPlRow = null;                                             //  временное кол-во пустого пространства в ряду
+    let tempFrPlRow = null;                                              //  временное кол-во пустого пространства в ряду
     let frPlRow = null;                                                 //  кол-во пустого простанста (могли бы быть парковочные места)
     let tempRowNum = null;                                              //  временное кол-во столбцов в ряду
 
     for(let tempPlInRow = 1; tempPlInRow < placesInRow; tempPlInRow++) {
-        tempFrPlRow = places % tempPlInRow;                             //  кол-во пустого простанства оставшиеся после создания парковочных мест
-        tempRowNum = Math.ceil(places / tempPlInRow);
+        tempFrPlRow = PLACES % tempPlInRow;                             //  кол-во пустого простанства оставшиеся после создания парковочных мест
+        tempRowNum = Math.ceil(PLACES / tempPlInRow);
 
         if((tempFrPlRow === 0) || (frPlRow <= tempFrPlRow && parkingRow > tempRowNum) || (frPlRow === null && parkingRow === null)) {
             parkingCol = tempPlInRow;
@@ -30,97 +42,139 @@ const createParkingSize = () => {
         }                     
     }
 }
+
 //  Функция подсчета свободных и занятых парковочных мест
 const countPlaces = () => {
-    parkingArr.forEach(place => {
-        (place.occupied) ? busyCount++ : freeCount++;
-    });
+    return parkingArr.reduce((sum, place) => {
+        if(!place.occupied) sum += 1;
+        return sum;
+    }, 0);
+}
 
-    freeSpaces.innerHTML = `Свободных мест = ${freeCount}`;
-    busyPlaces.innerHTML = `Занятых мест = ${busyCount}`;
+//  Функция обновления информационного окна
+const updateIfoBoard = () => {
+    freeSpaces.innerHTML = `Free places = ${countPlaces()}`;
+    occupiedSpaces.innerHTML = `Occupied places = ${PLACES - countPlaces()}`;
 }
 
 //  Функция отслеживания текущего времени
 const showCurTime = () => {
     setInterval(() => {
-        curTime.innerHTML = `Текущее время ${moment().format("HH:mm:ss")}`;
-    }, 10);
-  }
+        curTime.innerHTML = `${moment().format("HH:mm:ss")}`;
+    }, 100);
+}
+
+//  Отрисовка парковочных мест на поле
+const renderParkPlaces = () => {
+    mainEl.innerHTML = parkingArr.reduce((str, place) => {
+        return `${str}
+        <div id="${place.id}" class="place">
+        №${place.id}<br>
+        ${place.occupied ? "occupied" : "free"}<br>
+        ${place.time}<br>
+        </div>
+        `;
+    }, "");
+
+    // Создание массива парковочных мест
+    parkingPlacesArr = mainEl.querySelectorAll(".place");
+    
+    //  Определение размеров одного парковочного места
+    placeHeight = (document.documentElement.clientHeight - infoBoard.clientHeight) / parkingRow;
+    placeWidth = document.documentElement.clientWidth / parkingCol;
+    
+    // Присвоенение размеров парковочному месту
+    for(let i = 0; i < PLACES; i++) {
+        parkingPlacesArr[i].style.height = `${placeHeight}px`;
+        parkingPlacesArr[i].style.width = `${placeWidth}px`;
+        parkingPlacesArr[i].classList.add("center", "free");
+    }
+}
+
+//  Изменение параметров парковочного места
+const changePlaceParam = (i, str1, str2) => {
+    parkingArr[i].occupied = true;
+    (str1 === "free") ? parkingArr[i].time = parkingTime.value : parkingArr[i].time = "--:--";
+    parkingPlacesArr[i].classList.remove(str1);
+    parkingPlacesArr[i].classList.add(str2);
+    parkingPlacesArr[i].innerHTML = `
+                                            №${i}<br>
+                                            ${parkingArr[i].occupied ? "occupied" : "free"}<br>
+                                            ${parkingArr[i].time}<br>
+                                        `;
+    countPlaces();
+    updateIfoBoard();
+}
 
 //  Создание размеров стоянки
 createParkingSize();
-
-//  Создание поля для разметки парковки в html, задание размеров
-let parking = document.querySelector(".main");
-parking.style.height = `${document.documentElement.clientHeight - infoBoard.clientHeight}px`;
-parking.style.width = `${document.documentElement.clientWidth}px`;
-
-//  Создание объектов со свойствами в массиве и добавление их в html-разметку
-for(let i = 0; i < parkingArr.length; i++) {
-    parkingArr[i] = new Object();
-    parkingArr[i].id = i + 1;
-    parkingArr[i].occupied = false;
-    parkingArr[i].time = "00:00:00";
-    parking.innerHTML += `<div id="place-${i + 1}" class="place"></div>`;
-
-    if((i + 1) % parkingCol === 0 && i !== 0) {
-        parking.innerHTML += "<br>";
-    }
-}
-
-// Создание массива парковочных мест
-let parkingPlacesArr = parking.querySelectorAll(".place");
-
-//  Определение размеров парковочного места
-placeHeight = (document.documentElement.clientHeight - infoBoard.clientHeight) / parkingRow;
-// placeHeight = document.documentElement.clientHeight / parkingRow;
-placeWidth = document.documentElement.clientWidth / parkingCol;
-
-//  Отрисовка парковочных мест на поле
-for(let i = 0; i < parkingPlacesArr.length; i++) {
-    parkingPlacesArr[i].style.height = `${placeHeight}px`;
-    parkingPlacesArr[i].style.width = `${placeWidth}px`;
-    parkingPlacesArr[i].style.border = "1px solid black";
-    parkingPlacesArr[i].style.boxSizing = "border-box";
-    parkingPlacesArr[i].classList.add("center", "free");
-    parkingPlacesArr[i].innerHTML = `<div>ID = ${parkingArr[i].id}</div><div class="status">${(parkingArr[i].occupied) ? "Busy" : "Free"}</div><div class="timer">Place taken ${parkingArr[i].time}</div>`;
-}
-
+createPlaces();
 countPlaces();
 showCurTime();
+updateIfoBoard();
+
+//  Создание поля для разметки парковки в html
+renderParkPlaces();
+
+let startParking = document.querySelector(".car-in");
+let setTimeParking = document.querySelector(".btn-ok");
+let canSetTimeParking = document.querySelector(".btn-cancel");
+let stopParking = document.querySelector(".car-out");
+let freeParkingPlace = document.querySelector(".btn-yes");
+let noFreeParkingPlace = document.querySelector(".btn-no");
+
+
+//  Остлеживания ввода времени в появившемся окне
+let parkingTime = document.querySelector(".from-time");
 
 //   Отслеживаем нажатия на парковочные места
-parking.addEventListener("click", (clickEvent) => {
-    // console.dir(clickEvent);
-    // console.log(clickEvent.target.id);
-    let placeId = `#${clickEvent.target.id}`;
-    
-    if(clickEvent.target.classList.contains("free")) {
-        clickEvent.target.classList.remove("free");
-        clickEvent.target.classList.add("center", "busy");
-        clickEvent.target.occupied = "true";
-    }
-    
-    //  Блокируем возможность нажатия других парковочных мест, до окончания ввода времени бронирования места
-    let inputTime = document.querySelector(".car-in");
-    inputTime.classList.add("car-in-on");
+mainEl.addEventListener("click", (clickEvent) => {
+    let placeId = Number(clickEvent.target.id); 
     
     //  Остлеживания ввода времени в появившемся окне
-    let parkingTime = document.querySelector(".from-time");
-    parkingTime.value = moment().format("HH:mm:ss");
+    parkingTime.value = moment().format("HH:mm");
     
-    parkingTime.addEventListener("keydown", (keyEvent) => {
-        console.dir(keyEvent);
-        if(keyEvent.code === "Enter" || keyEvent.code === "NumpadEnter") {
-            inputTime.style.display = "none";
-            let place = document.querySelector(placeId);
-            // console.dir(divId);
-            let status = place.querySelector(".status");
-            status.innerHTML = "Busy";
+    if(clickEvent.target.classList.contains("free")) {
+        let frePlaces = countPlaces();
+        let minTimeLim = moment("09:00", "HH:mm");
+        let maxTimeLim = moment("18:00", "HH:mm");
+        let curParTime = moment();
 
-            let timer = place.querySelector(".timer");
-            timer.innerHTML = "Busy";
+        if(frePlaces < PLACES * parFillFact && (moment(curParTime).isAfter(minTimeLim) && moment(curParTime).isBefore(maxTimeLim))) {
+            let alert = startParking.querySelector(".alert");
+            alert.innerHTML = "We recommend not to occupy a parking space!";
+        };
+
+        //  Блокируем возможность нажатия других парковочных мест, до окончания ввода времени бронирования места
+        startParking.style.display = "flex";
+        
+        const clickBtnOk = () => {
+            changePlaceParam(placeId, "free", "busy");
+            startParking.style.display = "none";
+            setTimeParking.removeEventListener("click", clickBtnOk);
         }
-    });
 
+        setTimeParking.addEventListener("click", clickBtnOk);
+
+        canSetTimeParking.addEventListener("click", () => {
+            setTimeParking.removeEventListener("click", clickBtnOk);
+            startParking.style.display = "none";
+        });
+    }
+    else {
+        let occuTime = stopParking.querySelector(".occu-time");
+        let startParTime = moment(parkingArr[placeId].time, "HH:mm");
+        let stopParTime = moment();
+        occuTime.innerHTML = moment(stopParTime - startParTime).utc().format("HH:mm");
+        stopParking.style.display = "flex";
+
+        freeParkingPlace.addEventListener("click", () => {
+            changePlaceParam(placeId, "busy", "free");
+            stopParking.style.display = "none";
+        });
+    
+        noFreeParkingPlace.addEventListener("click", () => {
+            stopParking.style.display = "none";
+        });
+    }
 });
